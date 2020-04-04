b8001a0fe61663938c41f9778f7d108be282b0eb
JHNLWHD <aljhoenilw@gmail.com>
2020-04-04 14:33:18 +0800

    feat: Add initial homepage with app's theme setup already.

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        primaryColor: Colors.red[300],
+        accentColor: Colors.red[200],
+        fontFamily: 'Inter'
+      ),
+      home: MyHomePage(title: 'COVID-19 Tracker: Zamboanga City'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Tracking current statistics of confirmed COVID-19 cases and history in Zamboanga City',
+              style: TextStyle(
+                  color: Colors.red[300],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,),
+                  textAlign: TextAlign.center,
+            ),
+            Text(
+              'Coming soon...',
+              style: TextStyle(
+                  color: Colors.red[300],
+                  fontSize: 19.0,
+                  fontStyle: FontStyle.italic),
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
