870a741c684987224289c2a4a3223ce36603f16a
JHNLWHD <aljhoenilw@gmail.com>
2020-04-04 14:33:18 +0800

    feat: Add initial homepage with app's theme setup already.

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'pages/BottomNavigation.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
